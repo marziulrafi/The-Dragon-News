755ff6a3fef9b7247d172304d21c426b5955c9e8
K M Marziul Karim <marziulrafi@gmail.com>
2025-05-01 19:35:46 +0600

    date fns

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from '../assets/logo.png'
+import { format } from 'date-fns';
 const Header = () => {
     return (
         <div className='flex justify-center mt-6 flex-col items-center gap-3'>
-           <img className='w-[350px]' src={logo} alt="" />
-           <p>Journalism Without Fear or Favour</p>
+           <img className='w-[400px]' src={logo} alt="" />
+           <p className='text-accent'>Journalism Without Fear or Favour</p>
+            <p className='font-semibold'>{format(new Date(), "EEEE,")} <span className='text-accent'>{format(new Date(), "MMMM dd, yyyy")}</span></p>
         </div>
     );
 };
